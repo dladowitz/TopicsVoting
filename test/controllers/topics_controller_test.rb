41a4068f4a7d972f863961472bd61b0c2a8f36e9
David Ladowitz <david@ladowitz.com>
2025-07-01 00:08:38 -0700

    Add Topic model, controller, views, and tests for topic creation and listing

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -2,17 +2,17 @@ require "test_helper"
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get topics_index_url
+    get topics_url
     assert_response :success
   end
 
   test "should get new" do
-    get topics_new_url
+    get new_topic_url
     assert_response :success
   end
 
-  test "should get create" do
-    get topics_create_url
-    assert_response :success
+  test "should create topic" do
+    post topics_url, params: { topic: { name: "Test Topic", description: "Test description" } }
+    assert_redirected_to topics_url
   end
 end
