8d37056f885bfdd0bc7ab1d64546390149c1cb96
David Ladowitz <david@ladowitz.com>
2025-07-29 10:44:43 -0700

    Fix minispec tests

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,18 +1,30 @@
 require "test_helper"
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @socratic_seminar = socratic_seminars(:one)
+  end
+
   test "should get index" do
-    get topics_url
+    get socratic_seminar_topics_url(@socratic_seminar)
     assert_response :success
   end
 
   test "should get new" do
-    get new_topic_url
+    get new_socratic_seminar_topic_url(@socratic_seminar)
     assert_response :success
   end
 
   test "should create topic" do
-    post topics_url, params: { topic: { name: "Test Topic", description: "Test description" } }
-    assert_redirected_to topics_url
+    assert_difference("Topic.count") do
+      post socratic_seminar_topics_url(@socratic_seminar), params: { 
+        topic: { 
+          name: "Test Topic",
+          section_id: sections(:one).id
+        }
+      }
+    end
+
+    assert_redirected_to socratic_seminar_topics_url(@socratic_seminar)
   end
 end
