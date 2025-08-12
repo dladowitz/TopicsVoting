77adbf8cfd274743afee43b8bd2b0b33ff0c92a0
David Ladowitz <david@ladowitz.com>
2025-08-12 08:53:49 -0700

    Updating styles

---
+++ app/helpers/organizations_helper.rb
@@ -0,0 +1,8 @@
+module OrganizationsHelper
+  def country_options
+    ISO3166::Country.all.sort_by { |c| c.common_name || c.unofficial_names.first }.map do |country|
+      name = country.common_name || country.unofficial_names.first
+      [ "#{name} (#{country.alpha2})", country.alpha2 ]
+    end
+  end
+end
---
+++ config/initializers/countries.rb
@@ -0,0 +1,6 @@
+require "countries/global"
+
+# Configure the countries gem if needed
+# ISO3166.configure do |config|
+#   config.locales = [:en]
+# end
