b6fbeaf55f4cc6c75c6207140e878184ac42802b
David Ladowitz <david@ladowitz.com>
2025-08-21 08:37:19 -0700

    updating rake task to take topics_list_url

--- app/services/import_service.rb
+++ app/services/import_service.rb
@@ -2,18 +2,18 @@
 # @see lib/tasks/import_sections_and_topics.rake for the actual import logic
 class ImportService
   # Class method wrapper for instance method
-  # @param [String] seminar_number The seminar number to import
+  # @param [SocraticSeminar] socratic_seminar The seminar to import topics for
   # @return [Array<Boolean, String>] Success status and output message
-  def self.import_sections_and_topics(seminar_number)
-    new.import_sections_and_topics(seminar_number)
+  def self.import_sections_and_topics(socratic_seminar)
+    new.import_sections_and_topics(socratic_seminar)
   end
 
   # Imports sections and topics for a specific seminar
-  # @param [String] seminar_number The seminar number to import
+  # @param [SocraticSeminar] socratic_seminar The seminar to import topics for
   # @return [Array<Boolean, String>] Success status and output message
   # @note Executes a Rake task in a subprocess to perform the import
-  def import_sections_and_topics(seminar_number)
-    command = "cd #{Rails.root} && bin/rails \"import:import_sections_and_topics[#{seminar_number}]\" 2>&1"
+  def import_sections_and_topics(socratic_seminar)
+    command = "cd #{Rails.root} && bin/rails \"import:import_sections_and_topics[#{socratic_seminar.id}]\" 2>&1"
     output, status = Open3.capture2(command)
     [ status.success?, output ]
   end
--- lib/tasks/import_sections_and_topics.rake
+++ lib/tasks/import_sections_and_topics.rake
@@ -1,23 +1,21 @@
 SECTIONS_TO_SKIP = [ "intro" ]
 
 namespace :import do
-  desc "Import Sections and Topics from bitcoinbuildersf.com for a given builder_number"
-  task :import_sections_and_topics, [ :builder_number ] => :environment do |t, args|
+  desc "Import Sections and Topics from bitcoinbuildersf.com for a given socratic_seminar_id"
+  task :import_sections_and_topics, [ :socratic_seminar_id ] => :environment do |t, args|
     require "open-uri"
     require "nokogiri"
 
-    builder_number = args[:builder_number].to_s
-    builder_number_for_url = builder_number.rjust(2, "0")
-    url = "https://www.bitcoinbuildersf.com/builder-#{builder_number_for_url}/"
+    seminar = SocraticSeminar.find_by(id: args[:socratic_seminar_id])
+    if seminar.nil?
+      puts "No SocraticSeminar found with id=#{args[:socratic_seminar_id]}"
+      exit 1
+    end
+
+    url = seminar.topics_list_url
     puts "Fetching: #{url}"
     html = URI.open(url).read
     doc = Nokogiri::HTML(html)
-
-    seminar = SocraticSeminar.find_by(seminar_number: builder_number.to_i)
-    if seminar.nil?
-      puts "No SocraticSeminar found with seminar_number=#{builder_number}"
-      exit 1
-    end
 
     doc.css("h2").each do |h2|
       section_id = h2["id"]
