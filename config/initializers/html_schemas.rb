6f7ec64baa3e39dc1d989222d66a1f6caf43b9ee
David Ladowitz <david@ladowitz.com>
2025-08-22 13:04:28 -0700

    Fix autoload paths for schema classes

--- config/initializers/html_schemas.rb
+++ config/initializers/html_schemas.rb
@@ -1,3 +1,3 @@
 # Load HTML schema classes
-Rails.autoloaders.main.push_dir(Rails.root.join("app", "services", "html_schemas"))
+Rails.autoloaders.main.push_dir(Rails.root.join("app", "services"))
 require_relative "../../app/services/html_schemas"
