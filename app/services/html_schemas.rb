614d119d6342dd6e0655dd21e043d36996fb2e9a
David Ladowitz <david@ladowitz.com>
2025-08-22 13:01:02 -0700

    Use autoload for schema classes

--- app/services/html_schemas.rb
+++ app/services/html_schemas.rb
@@ -1,3 +1,6 @@
 # Namespace for HTML schema parsers
 module HtmlSchemas
+  autoload :BaseSchema, "html_schemas/base"
+  autoload :SFBitcoinDevsSchema, "html_schemas/sf_bitcoin_devs"
+  autoload :CDMXBitDevsSchema, "html_schemas/cdmx_bit_devs"
 end
