8ffb88c6c25274f24a9416502edadbacffa6f6c2
David Ladowitz <david@ladowitz.com>
2025-08-22 13:05:32 -0700

    Use absolute paths for schema autoloading

--- app/services/html_schemas.rb
+++ app/services/html_schemas.rb
@@ -1,6 +1,6 @@
 # Namespace for HTML schema parsers
 module HtmlSchemas
-  autoload :BaseSchema, "html_schemas/base"
-  autoload :SFBitcoinDevsSchema, "html_schemas/sf_bitcoin_devs"
-  autoload :CDMXBitDevsSchema, "html_schemas/cdmx_bit_devs"
+  autoload :BaseSchema, File.expand_path("html_schemas/base", __dir__)
+  autoload :SFBitcoinDevsSchema, File.expand_path("html_schemas/sf_bitcoin_devs", __dir__)
+  autoload :CDMXBitDevsSchema, File.expand_path("html_schemas/cdmx_bit_devs", __dir__)
 end
